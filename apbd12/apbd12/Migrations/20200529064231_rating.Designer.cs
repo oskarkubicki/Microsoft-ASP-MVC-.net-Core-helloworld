e6a07525b507ab53dff26fd3918d87285079cf7d
oskar k <s19732@pjwstk.edu.pl>
2020-05-29 09:29:24 +0200

    finished task 12

--- apbd12/apbd12/Migrations/20200529064231_rating.Designer.cs
+++ apbd12/apbd12/Migrations/20200529064231_rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using apbd12.Data;
 
 namespace apbd12.Migrations
 {
     [DbContext(typeof(apbd12Context))]
-    partial class apbd12ContextModelSnapshot : ModelSnapshot
+    [Migration("20200529064231_rating")]
+    partial class rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- apbd12/apbd12/Models/SeedData.cs
+++ apbd12/apbd12/Models/SeedData.cs
@@ -28,6 +28,7 @@ namespace apbd12.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -36,7 +37,8 @@ namespace apbd12.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "PG"
                     },
 
                     new Movie
@@ -44,7 +46,8 @@ namespace apbd12.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "PG-13"
                     },
 
                     new Movie
@@ -52,7 +55,8 @@ namespace apbd12.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "R"
                     }
                 );
                 context.SaveChanges();
